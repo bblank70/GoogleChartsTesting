d998a15c768d28275e624ed17bb721e5c2e51f36
Brian Blank <beblank@gmail.com>
2024-04-12 16:43:18 -0500

    provides support for debugging HTML while the webserver is running
    Working version of cached/non cached templates

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -8,7 +8,10 @@ import (
 
 // index is the home page handler
 func Index(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "base.html")
+	render.RenderTemplate(w, "base.html") ///used for debugging templates
+
+	// render.RenderCachedTemplates(w, "base.html") ///puts the template in production
+
 	// render.RenderTemplate(w, "base.html", Dataslice)
 	// tpl.ExecuteTemplate(w, "base.html", Dataslice)
 }
